6572316642f1ab7a6c9dbb59b7c776cddb8f06e5
Ninjaman255 <aaronclevette@gmail.com>
2025-05-31 20:27:35 -0500

    Added whitelisted mods and BBS support

---
+++ assets/tiles/BBSOldFlipped.tsx
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="BBSOldFlipped" tilewidth="48" tileheight="69" tilecount="3" columns="3" objectalignment="top">
+ <image source="BBSOldFlipped.png" width="144" height="69"/>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="38" y="69">
+    <polygon points="0,0 -38,-19 -28,-25 10,-6"/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="0" duration="330"/>
+   <frame tileid="1" duration="330"/>
+  </animation>
+ </tile>
+</tileset>

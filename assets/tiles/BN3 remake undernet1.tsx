a5b0bd8b8c46f88c2a8c5eeb69a9b7f2b876e62f
Ninjaman255 <aaronclevette@gmail.com>
2025-06-13 11:34:13 -0500

    added quetzens revamped undernet tileset

---
+++ assets/tiles/BN3 remake undernet1.tsx
@@ -0,0 +1,130 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="BN3 remake undernet1" tilewidth="64" tileheight="40" tilecount="168" columns="12">
+ <tileoffset x="0" y="8"/>
+ <grid orientation="orthogonal" width="64" height="32"/>
+ <image source="BN3 remake undernet1.png" width="768" height="560"/>
+ <tile id="15">
+  <animation>
+   <frame tileid="15" duration="200"/>
+   <frame tileid="27" duration="200"/>
+   <frame tileid="39" duration="200"/>
+   <frame tileid="51" duration="200"/>
+   <frame tileid="39" duration="200"/>
+   <frame tileid="27" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="19">
+  <animation>
+   <frame tileid="19" duration="200"/>
+   <frame tileid="31" duration="200"/>
+   <frame tileid="43" duration="200"/>
+   <frame tileid="55" duration="200"/>
+   <frame tileid="43" duration="200"/>
+   <frame tileid="31" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="23">
+  <animation>
+   <frame tileid="23" duration="200"/>
+   <frame tileid="35" duration="200"/>
+   <frame tileid="47" duration="200"/>
+   <frame tileid="59" duration="200"/>
+   <frame tileid="47" duration="200"/>
+   <frame tileid="35" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="63">
+  <animation>
+   <frame tileid="63" duration="200"/>
+   <frame tileid="75" duration="200"/>
+   <frame tileid="87" duration="200"/>
+   <frame tileid="99" duration="200"/>
+   <frame tileid="87" duration="200"/>
+   <frame tileid="75" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="67">
+  <animation>
+   <frame tileid="67" duration="200"/>
+   <frame tileid="79" duration="200"/>
+   <frame tileid="91" duration="200"/>
+   <frame tileid="103" duration="200"/>
+   <frame tileid="91" duration="200"/>
+   <frame tileid="79" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="71">
+  <animation>
+   <frame tileid="71" duration="200"/>
+   <frame tileid="83" duration="200"/>
+   <frame tileid="95" duration="200"/>
+   <frame tileid="107" duration="200"/>
+   <frame tileid="95" duration="200"/>
+   <frame tileid="83" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="120">
+  <animation>
+   <frame tileid="120" duration="200"/>
+   <frame tileid="132" duration="200"/>
+   <frame tileid="144" duration="200"/>
+   <frame tileid="156" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="121">
+  <animation>
+   <frame tileid="121" duration="200"/>
+   <frame tileid="133" duration="200"/>
+   <frame tileid="145" duration="200"/>
+   <frame tileid="157" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="122">
+  <animation>
+   <frame tileid="122" duration="200"/>
+   <frame tileid="134" duration="200"/>
+   <frame tileid="146" duration="200"/>
+   <frame tileid="158" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="123">
+  <animation>
+   <frame tileid="123" duration="200"/>
+   <frame tileid="135" duration="200"/>
+   <frame tileid="147" duration="200"/>
+   <frame tileid="159" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="124">
+  <animation>
+   <frame tileid="124" duration="200"/>
+   <frame tileid="136" duration="200"/>
+   <frame tileid="148" duration="200"/>
+   <frame tileid="160" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="125">
+  <animation>
+   <frame tileid="125" duration="200"/>
+   <frame tileid="137" duration="200"/>
+   <frame tileid="149" duration="200"/>
+   <frame tileid="161" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="126">
+  <animation>
+   <frame tileid="126" duration="200"/>
+   <frame tileid="138" duration="200"/>
+   <frame tileid="150" duration="200"/>
+   <frame tileid="162" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="127">
+  <animation>
+   <frame tileid="127" duration="200"/>
+   <frame tileid="139" duration="200"/>
+   <frame tileid="151" duration="200"/>
+   <frame tileid="163" duration="200"/>
+  </animation>
+ </tile>
+</tileset>

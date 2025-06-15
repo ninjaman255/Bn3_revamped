78319a191f9b4a340d3840088e0ad55de9b77e93
Ninjaman255 <aaronclevette@gmail.com>
2025-06-15 12:51:42 -0500

    Started tag map and modifications to keristeros tag script. made extended alpha dark panels to be full panel sizes.

---
+++ assets/tiles/alpha_sludge_tiles.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="alpha_sludge_tiles" tilewidth="64" tileheight="32" tilecount="8" columns="1">
+ <tileoffset x="0" y="6"/>
+ <image source="alpha_sludge_tiles.png" width="64" height="256"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="18">
+    <polygon points="0,-2 32,-18 64,-2 32,14"/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="0" duration="330"/>
+   <frame tileid="1" duration="330"/>
+   <frame tileid="2" duration="330"/>
+   <frame tileid="3" duration="330"/>
+   <frame tileid="4" duration="330"/>
+   <frame tileid="5" duration="330"/>
+   <frame tileid="6" duration="330"/>
+   <frame tileid="7" duration="330"/>
+  </animation>
+ </tile>
+</tileset>

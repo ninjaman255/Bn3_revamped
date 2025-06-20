8aa76b25e88996d6493aa439b27d6220bcbe3b60
Ninjaman255 <aaronclevette@gmail.com>
2025-06-19 21:28:39 -0500

    minor adjustments to secret net and water tiles to hopefully fix collissions, fixed layer error on a platform in undernet 2

--- assets/tiles/WaterTilesV2.tsx
+++ assets/tiles/WaterTilesV2.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.0" name="WaterTilesV2" tilewidth="64" tileheight="44" tilecount="36" columns="9">
+<tileset version="1.10" tiledversion="1.11.2" name="WaterTilesV2" tilewidth="64" tileheight="44" tilecount="36" columns="9">
  <tileoffset x="0" y="12"/>
  <image source="WaterTilesV2.png" width="576" height="176"/>
  <tile id="0">
@@ -36,6 +36,48 @@
    <frame tileid="3" duration="300"/>
   </animation>
  </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="18">
+    <polygon points="0,-2 32,-18 64,-2 32,14"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="18">
+    <polygon points="0,-2 32,-18 64,-2 32,14"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="18">
+    <polygon points="0,-2 32,-18 64,-2 32,14"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="18">
+    <polygon points="0,-2 32,-18 64,-2 32,14"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="18">
+    <polygon points="0,-2 32,-18 64,-2 32,14"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="18">
+    <polygon points="0,-2 32,-18 64,-2 32,14"/>
+   </object>
+  </objectgroup>
+ </tile>
  <tile id="9">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="18">
@@ -69,6 +111,48 @@
    <frame tileid="11" duration="300"/>
    <frame tileid="12" duration="300"/>
   </animation>
+ </tile>
+ <tile id="11">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="18">
+    <polygon points="0,-2 32,-18 64,-2 32,14"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="12">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="18">
+    <polygon points="0,-2 32,-18 64,-2 32,14"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="18">
+    <polygon points="0,-2 32,-18 64,-2 32,14"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="14">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="18">
+    <polygon points="0,-2 32,-18 64,-2 32,14"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="15">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="18">
+    <polygon points="0,-2 32,-18 64,-2 32,14"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="16">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="18">
+    <polygon points="0,-2 32,-18 64,-2 32,14"/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="18">
   <objectgroup draworder="index" id="2">
@@ -104,6 +188,48 @@
    <frame tileid="21" duration="300"/>
   </animation>
  </tile>
+ <tile id="20">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="18">
+    <polygon points="0,-2 32,-18 64,-2 32,14"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="21">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="18">
+    <polygon points="0,-2 32,-18 64,-2 32,14"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="22">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="18">
+    <polygon points="0,-2 32,-18 64,-2 32,14"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="23">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="18">
+    <polygon points="0,-2 32,-18 64,-2 32,14"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="24">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="18">
+    <polygon points="0,-2 32,-18 64,-2 32,14"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="25">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="18">
+    <polygon points="0,-2 32,-18 64,-2 32,14"/>
+   </object>
+  </objectgroup>
+ </tile>
  <tile id="27">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="18">
@@ -137,6 +263,48 @@
    <frame tileid="29" duration="300"/>
    <frame tileid="30" duration="300"/>
   </animation>
+ </tile>
+ <tile id="29">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="18">
+    <polygon points="0,-2 32,-18 64,-2 32,14"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="30">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="18">
+    <polygon points="0,-2 32,-18 64,-2 32,14"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="31">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="18">
+    <polygon points="0,-2 32,-18 64,-2 32,14"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="32">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="18">
+    <polygon points="0,-2 32,-18 64,-2 32,14"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="33">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="18">
+    <polygon points="0,-2 32,-18 64,-2 32,14"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="34">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="18">
+    <polygon points="0,-2 32,-18 64,-2 32,14"/>
+   </object>
+  </objectgroup>
  </tile>
  <wangsets>
   <wangset name="EmptySpaceFillin" type="mixed" tile="0">

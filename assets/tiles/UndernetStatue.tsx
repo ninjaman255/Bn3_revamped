a7e4c5622e11d785d246064ad4021bb44c300da4
Ninjaman255 <aaronclevette@gmail.com>
2025-06-20 23:11:03 -0500

    added some warps, updated some areas and linked up undernet sqr to undernet 3, Added undernet statue asset with modifications to help it appear better visually. Added Undernet-bbs and removed unneeded pdn file.

---
+++ assets/tiles/UndernetStatue.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.11.2" name="UndernetStatue" tilewidth="90" tileheight="108" tilecount="1" columns="1" objectalignment="bottomright">
+ <tileoffset x="0" y="32"/>
+ <image source="UndernetStatue.png" width="90" height="108"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="97" y="71">
+    <polygon points="3,1 -45,-22 -89,0 -43,24"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
---
+++ assets/tiles/undernet-bbs.tsx
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.11.2" name="undernet-bbs" tilewidth="48" tileheight="69" tilecount="3" columns="3" objectalignment="top">
+ <image source="undernet-bbs.png" width="144" height="69"/>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="8" y="70">
+    <polygon points="0,0 38,-19 28,-24 -10,-5"/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="2" duration="330"/>
+   <frame tileid="1" duration="330"/>
+  </animation>
+ </tile>
+</tileset>

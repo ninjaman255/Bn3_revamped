dd217c65c23ac88a81dab79a67d7cd025734ce87
Ninjaman255 <aaronclevette@gmail.com>
2025-06-20 23:35:21 -0500

    adding fire tileset and added it with pedestals to undernet sqr.

---
+++ assets/tiles/fire-pedestal.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.11.2" name="fire-pedestal" tilewidth="48" tileheight="64" tilecount="2" columns="2" objectalignment="bottom">
+ <tileoffset x="0" y="16"/>
+ <image source="fire-pedestal.png" width="96" height="64"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-8" y="50">
+    <polygon points="0,-2 32,-18 64,-2 32,14"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-8" y="50">
+    <polygon points="0,-2 32,-18 64,-2 32,14"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
---
+++ assets/tiles/obstacle_fire_1.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="obstacle fire 1" tilewidth="64" tileheight="128" tilecount="8" columns="4" objectalignment="bottom">
+ <tileoffset x="0" y="16"/>
+ <image source="obstacle_fire_1.png" width="256" height="256"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="128">
+    <polygon points="0,0 -32,-16 0,-32 32,-16"/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="0" duration="50"/>
+   <frame tileid="1" duration="50"/>
+   <frame tileid="2" duration="50"/>
+   <frame tileid="3" duration="50"/>
+   <frame tileid="4" duration="50"/>
+   <frame tileid="5" duration="50"/>
+   <frame tileid="6" duration="50"/>
+   <frame tileid="7" duration="50"/>
+  </animation>
+ </tile>
+</tileset>

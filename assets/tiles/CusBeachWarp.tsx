23e1993f9f600f3ac9cc31b3e1b3a10b6565088d
Ninjaman255 <aaronclevette@gmail.com>
2025-06-20 19:03:16 -0500

    adjusted collisions for beach warp to hopefully fix walk out. Fixed some missed Custom Warp typing on a warp or two in undernet2 to fix the warps.

--- assets/tiles/CusBeachWarp.tsx
+++ assets/tiles/CusBeachWarp.tsx
@@ -1,10 +1,10 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="CusBeachWarp" tilewidth="52" tileheight="34" tilecount="4" columns="4" objectalignment="top">
+<tileset version="1.8" tiledversion="1.11.2" name="CusBeachWarp" tilewidth="52" tileheight="34" tilecount="4" columns="4" objectalignment="top">
  <tileoffset x="-1" y="12"/>
  <image source="CusBeachWarp.png" width="208" height="34"/>
  <tile id="0">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="6" y="7" width="40" height="18">
+   <object id="1" x="15" y="11.4112" width="22" height="10.5">
     <ellipse/>
    </object>
   </objectgroup>

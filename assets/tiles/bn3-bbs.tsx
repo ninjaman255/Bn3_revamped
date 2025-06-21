bf9d4114638eb7d47198594554e6dab00a807db2
Ninjaman255 <aaronclevette@gmail.com>
2025-06-21 01:20:46 -0500

    fixed bbs boards to be better set on minimap and be a better tsx.

---
+++ assets/tiles/bn3-bbs.tsx
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.11.2" name="bn3-bbs" tilewidth="80" tileheight="96" tilecount="4" columns="2" objectalignment="top">
+ <image source="bn3-bbs.png" width="160" height="192"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="89">
+    <polygon points="0,4 32,-12 40,-8 8,8"/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="0" duration="330"/>
+   <frame tileid="1" duration="330"/>
+  </animation>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="89">
+    <polygon points="0,4 32,-12 40,-8 8,8"/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="2" duration="330"/>
+   <frame tileid="3" duration="330"/>
+  </animation>
+ </tile>
+</tileset>

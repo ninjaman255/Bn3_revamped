32df8e7b90cd18ef7eee4dcd03c1f0f8ef323474
Ninjaman255 <aaronclevette@gmail.com>
2025-06-22 11:16:56 -0500

    adjustments to secret area 1 and 2

---
+++ assets/tiles/Bn3UndernetFlashingWarpVariant2.tsx
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.11.2" name="Bn3UndernetFlashingWarpVariant2" tilewidth="44" tileheight="22" tilecount="6" columns="6" objectalignment="top">
+ <tileoffset x="0" y="5"/>
+ <image source="Bn3UndernetFlashingWarpVariant2.png" width="264" height="22"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="11">
+    <polygon points="0,0 22,-11 44,0 22,11"/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="0" duration="300"/>
+   <frame tileid="1" duration="300"/>
+   <frame tileid="2" duration="300"/>
+   <frame tileid="3" duration="300"/>
+   <frame tileid="4" duration="300"/>
+   <frame tileid="5" duration="300"/>
+  </animation>
+ </tile>
+</tileset>

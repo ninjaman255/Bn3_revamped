3e05a0e11acfb990ade29c6c9866a8679e43560b
Ninjaman255 <aaronclevette@gmail.com>
2025-06-21 19:23:42 -0500

    small adjustment to the colissions on elephant slide and play dome to be smoother in game.

--- assets/tiles/Cyber Elephant Slide.tsx
+++ assets/tiles/Cyber Elephant Slide.tsx
@@ -1,11 +1,11 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="Cyber Elephant Slide" tilewidth="36" tileheight="30" tilecount="3" columns="3" objectalignment="bottom">
+<tileset version="1.8" tiledversion="1.11.2" name="Cyber Elephant Slide" tilewidth="36" tileheight="30" tilecount="3" columns="3" objectalignment="bottom">
  <tileoffset x="0" y="8"/>
  <image source="Cyber Elephant Slide.png" width="108" height="30"/>
  <tile id="0">
   <objectgroup draworder="index" id="3">
-   <object id="2" x="18" y="30">
-    <polygon points="0,0 -17.5455,-13.0909 -8.72727,-19.4545 4.09091,-18.3636 17.5455,-7.18182 18,-5.63636 10.4545,0"/>
+   <object id="2" x="16" y="31">
+    <polygon points="-20,-14 -2,-23 26,-8 8,1"/>
    </object>
   </objectgroup>
   <animation>
--- assets/tiles/Cyber Play Dome.tsx
+++ assets/tiles/Cyber Play Dome.tsx
@@ -1,11 +1,11 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="Cyber Play Dome" tilewidth="58" tileheight="46" tilecount="3" columns="3" objectalignment="bottom">
+<tileset version="1.8" tiledversion="1.11.2" name="Cyber Play Dome" tilewidth="58" tileheight="46" tilecount="3" columns="3" objectalignment="bottom">
  <tileoffset x="0" y="24"/>
  <image source="Cyber Play Dome.png" width="174" height="46"/>
  <tile id="0">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="14.5" y="46">
-    <polygon points="0,0 -14.5,-11.5 -14.5,-23 0,-34.5 29,-34.5 43.5,-23 43.5,-11.5 29,0 0,0"/>
+   <object id="2" x="-3" y="10" width="64" height="39">
+    <ellipse/>
    </object>
   </objectgroup>
   <animation>

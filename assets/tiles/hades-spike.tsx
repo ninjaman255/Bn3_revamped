2a3c2e8768a2831c52298cbe954a89645d4404c1
Ninjaman255 <aaronclevette@gmail.com>
2025-06-30 17:45:19 -0500

    another fix to drawing offset/orientation.

--- assets/tiles/hades-spike.tsx
+++ assets/tiles/hades-spike.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.8" tiledversion="1.11.2" name="hades-spike" tilewidth="64" tileheight="64" tilecount="1" columns="1" objectalignment="bottom">
- <tileoffset x="0" y="48"/>
+ <tileoffset x="0" y="12"/>
  <image source="hades-spike.png" width="64" height="64"/>
  <tile id="0">
   <objectgroup draworder="index" id="2">

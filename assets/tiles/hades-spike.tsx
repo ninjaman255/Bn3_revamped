cec29d1c5329b71af9848025b203fb54e7ea6bb5
Ninjaman255 <aaronclevette@gmail.com>
2025-06-30 17:38:32 -0500

    fix hades-spike drawing orientation.

--- assets/tiles/hades-spike.tsx
+++ assets/tiles/hades-spike.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.11.2" name="hades-spike" tilewidth="64" tileheight="64" tilecount="1" columns="1">
+<tileset version="1.8" tiledversion="1.11.2" name="hades-spike" tilewidth="64" tileheight="64" tilecount="1" columns="1" objectalignment="bottom">
  <tileoffset x="0" y="32"/>
  <image source="hades-spike.png" width="64" height="64"/>
  <tile id="0">

79a7a0c9d27dc3bc4a4d7b5c9768331545c4979f
Ninjaman255 <aaronclevette@gmail.com>
2025-06-30 17:35:05 -0500

    added hades spikes where needed.

---
+++ assets/tiles/hades-spike.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.11.2" name="hades-spike" tilewidth="64" tileheight="64" tilecount="1" columns="1">
+ <tileoffset x="0" y="32"/>
+ <image source="hades-spike.png" width="64" height="64"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="32" y="64">
+    <polygon points="0,0 32,-16 0,-32 -32,-16"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>

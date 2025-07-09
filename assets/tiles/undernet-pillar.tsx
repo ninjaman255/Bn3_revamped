7542da347172e44fe582bf44d2a7f18a6051e249
Ninjaman255 <aaronclevette@gmail.com>
2025-07-09 01:02:05 -0500

    Updated undernet 4 with undernet pillar objects.

---
+++ assets/tiles/undernet-pillar.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.11.2" name="undernet-pillar" tilewidth="44" tileheight="100" tilecount="3" columns="3">
+ <image source="undernet-pillar.png" width="132" height="100"/>
+</tileset>

0ae95c0425d073d3bd9608bea2f28dbd32cf3e13
Ninjaman255 <aaronclevette@gmail.com>
2025-07-12 10:03:27 -0500

    fix collissions for undernet pillar

--- assets/tiles/undernet-pillar.tsx
+++ assets/tiles/undernet-pillar.tsx
@@ -1,4 +1,11 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.11.2" name="undernet-pillar" tilewidth="44" tileheight="100" tilecount="3" columns="3">
+<tileset version="1.10" tiledversion="1.11.2" name="undernet-pillar" tilewidth="44" tileheight="100" tilecount="3" columns="3">
  <image source="undernet-pillar.png" width="132" height="100"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="21" y="72">
+    <polygon points="1,4 -31,20 1,36 33,20"/>
+   </object>
+  </objectgroup>
+ </tile>
 </tileset>

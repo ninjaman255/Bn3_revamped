00099b5aef4de26234d4fc91c5d34626b72d97fa
Ninjaman255 <aaronclevette@gmail.com>
2025-08-30 09:16:22 -0500

    advertise_server fixes hopefully

--- scripts/advertise_server/main.lua
+++ scripts/advertise_server/main.lua
@@ -1,14 +1,14 @@
-local json = require('scripts/advertise_server/json')
-local base64 = require('scripts/advertise_server/base64')
-local urlencode = require('scripts/advertise_server/urlencode')
-local folder_path = "scripts/advertise_server/data/"
-local advertisement_json_path = folder_path.."advertisement.json"
-local secret_keys_json_path = folder_path.."secret_keys.json"
+local json = require('json')
+local base64 = require('base64')
+local urlencode = require('urlencode')
+local folder_path = "data/"
+local advertisement_json_path = folder_path .. "advertisement.json"
+local secret_keys_json_path = folder_path .. "secret_keys.json"
 local minimum_sync_interval = 5
-local maximum_sync_interval = 60*60
+local maximum_sync_interval = 60 * 60
 local public_info = {
-    pending_fields={},
-    time_since_last_sync={}
+    pending_fields = {},
+    time_since_last_sync = {}
 }
 local listservers = {}
 local secret_keys = {}
@@ -25,13 +25,13 @@ local function async(p)
 end
 local function await(v) return Async.await(v) end
 local function load_image_data(path)
-    print('[advertise_server] loading icon ',path)
+    print('[advertise_server] loading icon ', path)
     local f = io.open(path, "rb")
     local icon_data = f:read("*a")
     f:close()
     return icon_data
 end
-local function save_image_data(path,data)
+local function save_image_data(path, data)
     local f = io.open(path, "wb")
     f:write(data)
     f:flush()
@@ -40,33 +40,34 @@ end
 
 --Event handlers
 Net:on("player_connect", function(event)
-    player_maps[event.player_id] = 'default'--TODO this does not work correctly if the player is transfered elsewhere while joining
-    set_pending_field_for_all_servers("player_maps",player_maps)
+    player_maps[event.player_id] =
+    'default'                                --TODO this does not work correctly if the player is transfered elsewhere while joining
+    set_pending_field_for_all_servers("player_maps", player_maps)
 end)
 
 Net:on("player_disconnect", function(event)
     player_maps[event.player_id] = nil
-    set_pending_field_for_all_servers("player_maps",player_maps)
+    set_pending_field_for_all_servers("player_maps", player_maps)
 end)
 
 Net:on("player_area_transfer", function(event)
     local player_area = Net.get_player_area(event.player_id)
     player_maps[event.player_id] = player_area
-    set_pending_field_for_all_servers("player_maps",player_maps)
+    set_pending_field_for_all_servers("player_maps", player_maps)
 end)
 
-function set_pending_field_for_all_servers(field_name,value)
+function set_pending_field_for_all_servers(field_name, value)
     for server_id, _ in pairs(public_info.time_since_last_sync) do
-        set_pending_field(server_id,field_name,value)
+        set_pending_field(server_id, field_name, value)
     end
 end
 
-function set_pending_field(server_id,field_name,value)
+function set_pending_field(server_id, field_name, value)
     if not public_info.pending_fields[server_id] then
         public_info.pending_fields[server_id] = {}
     end
     public_info.pending_fields[server_id][field_name] = value
-    public_info.time_since_last_sync[server_id] = maximum_sync_interval+1
+    public_info.time_since_last_sync[server_id] = maximum_sync_interval + 1
 end
 
 function clear_pending_fields(server_id)
@@ -75,8 +76,8 @@ end
 
 function build_payload(server_id)
     local payload = {
-        server_id=server_id,
-        fields={}
+        server_id = server_id,
+        fields = {}
     }
     for field_name, value in pairs(public_info.pending_fields[server_id]) do
         payload.fields[field_name] = value
@@ -85,7 +86,7 @@ function build_payload(server_id)
     return payload
 end
 
-function get_secret_key(server_id,listserver_name)
+function get_secret_key(server_id, listserver_name)
     if not secret_keys[listserver_name] then
         return nil
     end
@@ -95,8 +96,8 @@ function get_secret_key(server_id,listserver_name)
     return secret_keys[listserver_name][server_id]
 end
 
-function save_secret_key(file_path,server_id,listserver_name,secret_key)
-    return async(function ()
+function save_secret_key(file_path, server_id, listserver_name, secret_key)
+    return async(function()
         if not secret_keys[listserver_name] then
             secret_keys[listserver_name] = {}
         end
@@ -104,19 +105,19 @@ function save_secret_key(file_path,server_id,listserver_name,secret_key)
             secret_keys[listserver_name][server_id] = {}
         end
         secret_keys[listserver_name][server_id] = secret_key
-        await(Async.write_file(file_path,json.encode(secret_keys)))
+        await(Async.write_file(file_path, json.encode(secret_keys)))
     end)
 end
 
 function sync_to_servers(server_id)
-    return async(function ()
-        print('[advertise_server] syncing '..server_id..' to server lists...')
+    return async(function()
+        print('[advertise_server] syncing ' .. server_id .. ' to server lists...')
         local payload = build_payload(server_id)
         local headers = {}
         headers["Content-Type"] = "application/json"
         --for each serverlist server
         for listserver_name, url in pairs(listservers) do
-            payload.secret_key = get_secret_key(server_id,listserver_name)
+            payload.secret_key = get_secret_key(server_id, listserver_name)
             local res = await(Async.request(url, {
                 method = "POST",
                 headers = headers,
@@ -125,7 +126,7 @@ function sync_to_servers(server_id)
             if res then
                 local data = json.decode(res.body)
                 if data and data.secret_key ~= nil then
-                    await(save_secret_key(secret_keys_json_path,server_id,listserver_name,data.secret_key))
+                    await(save_secret_key(secret_keys_json_path, server_id, listserver_name, data.secret_key))
                 end
             end
         end
@@ -134,7 +135,7 @@ function sync_to_servers(server_id)
 end
 
 local function read_json(file_path)
-    return async(function ()
+    return async(function()
         local data = {}
         pcall(function()
             data = json.decode(await(Async.read_file(file_path)))
@@ -147,12 +148,12 @@ local function read_json(file_path)
 end
 
 local function load_all_images_for_advertisements(advertisements)
-    return async(function ()
+    return async(function()
         for i, advertisement in ipairs(advertisements) do
             if advertisement.icon then
-                local image_path = "./"..folder_path..advertisement.icon
+                local image_path = "/" .. folder_path .. advertisement.icon
                 local image_data = load_image_data(image_path)
-                set_pending_field(advertisement.unique_server_id,"b64_image",base64.encode(image_data))
+                set_pending_field(advertisement.unique_server_id, "b64_image", base64.encode(image_data))
             end
         end
     end)
@@ -163,7 +164,7 @@ local function initialize_pending_fields_from_advertisements(advertisements)
         --Some ephemral fields need defaults
         local server_id = advertisement.unique_server_id
         for field_name, value in pairs(advertisement) do
-            set_pending_field(server_id,field_name,value)
+            set_pending_field(server_id, field_name, value)
         end
     end
 end
@@ -175,26 +176,27 @@ local function build_server_map(areas)
         if not server_map[area_id] then
             --initialize server map
             server_map[area_id] = {
-                name=area_p["Name"],
-                l={},--local connections (same server)
-                r={}--remote connections (other servers)
+                name = area_p["Name"],
+                l = {}, --local connections (same server)
+                r = {} --remote connections (other servers)
             }
         end
         local map_info = server_map[area_id]
         local objects = Net.list_objects(area_id)
         for j, object_id in ipairs(objects) do
-            local object = Net.get_object_by_id(area_id,object_id)
+            local object = Net.get_object_by_id(area_id, object_id)
             local custom_p = object.custom_properties
             --record map connections
             --local (ezlibs warps)
             if custom_p["Target Area"] then
-                map_info.l[custom_p["Target Area"]] = {id=custom_p["Target Object"]}
+                map_info.l[custom_p["Target Area"]] = { id = custom_p["Target Object"] }
             end
             --remote (server warps)
             local address = custom_p["Address"]
             local port = custom_p["Port"]
             if address and port then
-                map_info.r[address..":"..port] = {data=custom_p["Data"],incoming_data=custom_p["Incoming Data"]}
+                map_info.r[address .. ":" .. port] = { data = custom_p["Data"], incoming_data = custom_p
+                ["Incoming Data"] }
             end
             --record extra map info
             if object.class == "Shop" then
@@ -216,7 +218,7 @@ function advertise_map_if_it_changed(advertisements)
     local server_map = build_server_map(areas)
     for i, advertisement in ipairs(advertisements) do
         if advertisement.advertise_map then
-            set_pending_field(advertisement.unique_server_id,"map",server_map)
+            set_pending_field(advertisement.unique_server_id, "map", server_map)
         end
     end
     last_map_list = areas
